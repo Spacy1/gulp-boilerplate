17adf290c7d1fe18c9e9610175e9bc989bc88838
Zerk <axzerk@gmail.com>
2017-11-06 15:45:19 +0200

    removed default sass files and added sass partials support

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Gulp Project Boilerplate
+# Gulp Project Boilerplate
+
+### TODO
+
+- Add svg minification - https://www.npmjs.com/package/gulp-svgmin
+- Add svg sprites
+- Add ESLint
--- gulpfile.js
+++ gulpfile.js
@@ -19,13 +19,15 @@ const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const rigger = require('gulp-rigger');
 const htmlmin = require('gulp-htmlmin');
+const cachebust = require('gulp-cache-bust');
+
 
 // Project paths
 const paths = {
   src: {
     html: './src/*.html',
     js: './src/js/**/*.js',
-    css: ['src/sass/core/*.scss', './src/sass/**/*.scss'],
+    css: './src/sass/main.scss',
     img: './src/img/**/*.+(png|jpg|gif|svg)',
     fonts: './src/fonts/**/*.*'
   },
@@ -37,7 +39,8 @@ const paths = {
     fonts: './dist/fonts'
   },
   watch: {
-    html: './src/**/*.html'
+    html: './src/**/*.html',
+    css: './src/sass/**/*.scss',
   },
   clean: './dist'
 };
@@ -47,32 +50,28 @@ gulp.task('bundleHtml', () => {
   return gulp.src(paths.src.html)
     .pipe(gulpCached('html'))
     .pipe(rigger())
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(cachebust({ type: 'timestamp' }))
     .pipe(gulp.dest(paths.dist.html))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // Assembling .scss files
 gulp.task('bundleCss', () => {
   return gulp.src(paths.src.css)
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: ':nested'
-    }).on('error', sass.logError))
+    .pipe(sass({ outputStyle: ':nested' }).on('error', sass.logError))
     .pipe(concat('styles.min.css'))
     .pipe(autoprefixer({
-      browsers: ['last 5 versions', '> 1%'],
+      browsers: ['last 2 versions', '> 1%'],
       cascade: false
     }))
     .pipe(mmq())
     .pipe(cssnano())
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('.'))
+    .pipe(cachebust({ type: 'timestamp' }))
     .pipe(gulp.dest(paths.dist.css))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // Assembling .js files
@@ -81,15 +80,11 @@ gulp.task('bundleJs', () => {
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(concat('scripts.min.js'))
-    .pipe(babel({
-      presets: ['env']
-    }))
+    .pipe(babel({ presets: ['env'] }))
     .pipe(uglify())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.dist.js))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // Optimizing images
@@ -98,11 +93,10 @@ gulp.task('bundleImg', () => {
     .pipe(cache(imagemin([
       imagemin.gifsicle(),
       imagemin.jpegtran(),
-      imagemin.optipng()])))
+      imagemin.optipng()
+    ])))
     .pipe(gulp.dest(paths.dist.img))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 // Bundling fonts
@@ -112,16 +106,16 @@ gulp.task('bundleFonts', () => {
 });
 
 // Watching for changes in src files
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   gulp.watch(paths.watch.html, ['bundleHtml']);
-  gulp.watch(paths.src.css, ['bundleCss']);
+  gulp.watch(paths.watch.css, ['bundleCss']);
   gulp.watch(paths.src.js, ['bundleJs']);
   gulp.watch(paths.src.img, ['bundleImg']);
   gulp.watch(paths.src.fonts, ['bundleFonts']);
 });
 
 // BrowserSync server
-gulp.task('webServer', function () {
+gulp.task('webServer', () => {
   browserSync({
     server: {
       baseDir: './dist'
@@ -134,7 +128,7 @@ gulp.task('webServer', function () {
 });
 
 // Cleaning dist dir
-gulp.task('clean:dist', function () {
+gulp.task('clean:dist', () => {
   return del.sync(paths.clean);
 });
 
